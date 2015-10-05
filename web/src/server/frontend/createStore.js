fb5f8fdbff17b6cc411c88f6930128a06a100ece
Sergii <hello@webkadabra.com>
2015-10-05 04:27:52 +0300

    cleanup & remove brainfock stuff

--- web/src/server/frontend/createStore.js
+++ web/src/server/frontend/createStore.js
@@ -4,7 +4,7 @@ import initialState from '../initialState';
 import {configureStore} from '@este/common';
 import {fromJS} from 'immutable';
 import {mapDispatchToProps} from '@este/common';
-import loopback from 'loopback';
+//import loopback from 'loopback';
 import app from '../main';
 
 import createRoutes from '../../client/createRoutes.js';
@@ -13,29 +13,25 @@ import {RoutingContext, match} from 'react-router';
 
 export default function createStore(req) {
   return new Promise((resolve, reject) => {
+    
+    resolve(store);
+  });
+}
 
-    var ctx = loopback.getCurrentContext();
-    var currentUser = ctx && ctx.get('currentUser');
-    var _state ={
+export default function createStore(req) {
+  return new Promise((resolve, reject) => {
+
+    const requestState = fromJS(initialState).mergeDeep({
       device: {
         isMobile: ['phone', 'tablet'].indexOf(req.device.type) > -1
-      },
-      users: {
-        viewer: currentUser ? ctx.get('currentUser') : null
       }
-    };
-
-    // provide auth token id to client
-    if(currentUser) {
-      _state.users.viewer.authToken = ctx.get('accessToken').id;
-    }
-
-    actions.setAppBaseUrl(app.get('baseUrl'));
-
-    const requestState = fromJS(initialState).mergeDeep(_state);
+    });
     const store = configureStore(requestState.toJS());
     const {actions} = mapDispatchToProps(store.dispatch);
 
+    actions.addTodo(new Todo({title: 'relax'}));
+	
+    // resolve component's props
     const routes = createRoutes(() => store.getState());
     const location = createLocation(req.url);
 
@@ -63,7 +59,7 @@ export default function createStore(req) {
         resolveProps.actions = actions;
 
         let resolvingComponent = new renderProps.routes[1].component;
-
+        
         /**
          * Component has to have `resolveData` method, example:
          *
